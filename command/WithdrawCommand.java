d23623702be6bab3c351da8b3346b613068a8682
cyberGestalter <40896211+cyberGestalter@users.noreply.github.com>
2020-03-19 11:35:03 +0300

    Update WithdrawCommand.java

--- command/WithdrawCommand.java
+++ command/WithdrawCommand.java
@@ -12,42 +12,10 @@ import java.util.*;
 //Снятие денег
 class WithdrawCommand implements Command {
 
-    //private ResourceBundle res = ResourceBundle.getBundle(CashMachine.class.getPackage().getName() +".resources.withdraw_en", new Locale("EN"));
     private ResourceBundle res = ResourceBundle.getBundle(CashMachine.RESOURCE_PATH +"withdraw_en");
 
     @Override
     public void execute() throws InterruptOperationException {
-        /*String currencyCode = ConsoleHelper.askCurrencyCode();
-        CurrencyManipulator manipulator = CurrencyManipulatorFactory.getManipulatorByCurrencyCode(currencyCode);
-        while (true) {
-            ConsoleHelper.writeMessage("Введите сумму для вывода:");
-            int withdrawSumm = 0;
-            while (true) {
-                String withdrawSummString = ConsoleHelper.readString();
-                try {
-                    withdrawSumm = Integer.parseInt(withdrawSummString);
-                } catch (NumberFormatException e) {
-                    ConsoleHelper.writeMessage("Неверный формат ввода. Введите число");
-                    continue;
-                }
-                if (manipulator.isAmountAvailable(withdrawSumm)) {
-                    //Множество выданных банкнот
-                    Map <Integer, Integer> residueBalance = manipulator.withdrawAmount(withdrawSumm);
-
-                    List<Integer> residueBalanceKeys = new ArrayList<>(residueBalance.keySet());
-                    Collections.sort(residueBalanceKeys);
-                    for (Integer key : residueBalanceKeys) {
-                        ConsoleHelper.writeMessage("\t" + key + " - " + residueBalance.get(key));
-                    }
-                    ConsoleHelper.writeMessage("Транзакция проведена успешно");
-                    break;
-                } else {
-                    ConsoleHelper.writeMessage("На счету недостаточно средств. Введите меньшую сумму для вывода");
-                }
-            }
-        }*/
-
-        //CurrencyManipulator currencyManipulator = CurrencyManipulatorFactory.getManipulatorByCurrencyCode(ConsoleHelper.askCurrencyCode());
         ConsoleHelper.writeMessage(res.getString("before"));
         String currencyCode = ConsoleHelper.askCurrencyCode();
         CurrencyManipulator currencyManipulator = CurrencyManipulatorFactory.getManipulatorByCurrencyCode(currencyCode);
@@ -57,10 +25,7 @@ class WithdrawCommand implements Command {
         TreeMap<Integer, Integer> resultMap = new TreeMap<>(Comparator.reverseOrder());
 
         do {
-            //ConsoleHelper.writeMessage("Пожалуйста, введите сумму: ");
-
             ConsoleHelper.writeMessage(res.getString("specify.amount"));
-
             String str = ConsoleHelper.readString();
             try{
                 summ = Integer.parseInt(str);
@@ -73,12 +38,9 @@ class WithdrawCommand implements Command {
                     System.out.println("\t"+entry.getKey()+" - "+entry.getValue());
                     ConsoleHelper.writeMessage(String.format(res.getString("success.format"), entry.getKey()*entry.getValue(), currencyCode));
                 }
-                //ConsoleHelper.writeMessage("Транзакция завершена успешно!");
-
                 isOk = false;
                 break;
             }catch (NotEnoughMoneyException | NumberFormatException e){
-                //ConsoleHelper.writeMessage("Не удается набрать заказанную сумму( Попробуйте сумму кратную имеющимся номиналам!");
                 ConsoleHelper.writeMessage(res.getString("not.enough.money") + " or " + res.getString("exact.amount.not.available"));
                 ConsoleHelper.writeMessage(res.getString("specify.not.empty.amount"));
             }
